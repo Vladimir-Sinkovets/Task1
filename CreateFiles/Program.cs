04c51ecc27014752b94d8014089a1171bc7bc7a1
vladimir-sinkovets <vovasin49@gmail.com>
2022-12-01 22:35:36 +0300

    ended CreateFile

---
+++ CreateFiles/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CreateFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files\\");
+
+            FileCreator fileCreator = new();
+
+            fileCreator.Create(path, 100);
+        }
+    }
+}
